64158fe48022d96d6493bd2e938888f5f211b777
Frostfell <ichernyshov@bk.ru>
2018-08-30 07:50:35 +0300

    Updated pods

--- vkontakte-client/Controller/NewsFeedViewController.swift
+++ vkontakte-client/Controller/NewsFeedViewController.swift
@@ -16,7 +16,7 @@ class NewsFeedViewController: UITableViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    tableView.estimatedRowHeight = 600; // Fixes bug when table view jumps up on tableview.reloadData() call
+    tableView.estimatedRowHeight = 600; // Fixes bug when table view jumps up on tableView.reloadData() call
     NewsFeedService.instance.requestUsersNewsFeed()
     pairTableAndRealm()
   }
